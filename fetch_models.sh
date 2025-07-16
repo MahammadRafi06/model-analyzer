1559534e416c35daac0ed3193ecdc30d695c607f
Mahammad Rafi <mrafi@uw.edu>
2025-07-16 11:52:54 -0700

    Update fetch_models.sh

--- fetch_models.sh
+++ fetch_models.sh
@@ -29,7 +29,7 @@ set -ex
 
 # Convert Tensorflow inception V3 module to ONNX
 # Pre-requisite: Python3, venv, and Pip3 are installed on the system
-mkdir -p model_repository/inception_onnx/1
+mkdir -p inception_onnx/1
 wget -O /tmp/inception_v3_2016_08_28_frozen.pb.tar.gz \
      https://storage.googleapis.com/download.tensorflow.org/models/inception_v3_2016_08_28_frozen.pb.tar.gz
 (cd /tmp && tar xzf inception_v3_2016_08_28_frozen.pb.tar.gz)
@@ -38,10 +38,10 @@ source ./tf2onnx/bin/activate
 pip3 install "numpy<2" tensorflow tf2onnx
 python3 -m tf2onnx.convert --graphdef /tmp/inception_v3_2016_08_28_frozen.pb --output inception_v3_onnx.model.onnx --inputs input:0 --outputs InceptionV3/Predictions/Softmax:0
 deactivate
-mv inception_v3_onnx.model.onnx model_repository/inception_onnx/1/model.onnx
+mv inception_v3_onnx.model.onnx inception_onnx/1/model.onnx
 
 
 # ONNX densenet
-mkdir -p model_repository/densenet_onnx/1
-wget -O model_repository/densenet_onnx/1/model.onnx \
+mkdir -p densenet_onnx/1
+wget -O densenet_onnx/1/model.onnx \
      https://github.com/onnx/models/raw/main/validated/vision/classification/densenet-121/model/densenet-7.onnx
